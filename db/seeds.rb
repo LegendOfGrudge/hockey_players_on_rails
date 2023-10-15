4ca4874995de2dd43ab99a5ef25565e69c7f73f2
Nick <nicholasfletcher3682@yahoo.ca>
2023-10-15 15:29:11 -0500

    Added Team Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=false&isGame=false&sort=[{"property":"points","direction":"DESC"},{"property":"goals","direction":"DESC"},{"property":"assists","direction":"DESC"},{"property":"playerId","direction":"ASC"}]&start=0&limit=50&factCayenneExp=gamesPlayed>=1&cayenneExp=gameTypeId=2 and seasonId<=20222023 and seasonId>=20222023
+
+# nethttp.rb
+require 'uri'
+require 'net/http'
+require 'json'
+
+Team.delete_all
+
+uri = URI('https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=false&isGame=false&sort=[{"property":"points","direction":"DESC"},{"property":"goals","direction":"DESC"},{"property":"assists","direction":"DESC"},{"property":"playerId","direction":"ASC"}]&start=0&limit=200&factCayenneExp=gamesPlayed>=1&cayenneExp=gameTypeId=2 and seasonId<=20222023 and seasonId>=20222023')
+res = Net::HTTP.get_response(uri)
+data = JSON.parse(res.body)
+data["data"].each do |player|
+  teams = player["teamAbbrevs"].split(",").map(&:strip)
+
+  teams.each do |team_name|
+    team = Team.find_or_create_by(teamAbbrevs: team_name)
+  end
+end
+
+puts "Created #{Team.count} Teams"
